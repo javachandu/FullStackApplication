72e0d3bd303090b460c36cce4156eab1a82c3a3d
cdamarasinghu <chandrasekhar.damarasinghu@jnettechnologies.com>
2019-12-11 19:41:16 +0530

    first commit

---
+++ SpringFullStack/src/main/java/com/chandu/ppmtool/SpringFullStack/SpringFullStackApplication.java
@@ -0,0 +1,13 @@
+package com.chandu.ppmtool.SpringFullStack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFullStackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFullStackApplication.class, args);
+	}
+
+}
---
+++ SpringFullStack/src/test/java/com/chandu/ppmtool/SpringFullStack/SpringFullStackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chandu.ppmtool.SpringFullStack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFullStackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
